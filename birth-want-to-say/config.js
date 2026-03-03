2367cc5dc8bece3f6be82a8895459959aeaf2c42
alisascut-hue <alisa.scut@gmail.com>
2026-03-03 21:09:16 +0800

    Update config.js

--- birth-want-to-say/config.js
+++ birth-want-to-say/config.js
@@ -4,20 +4,22 @@ var config = {
     // 每句话尽量不要超过15个字,不然展示效果可能不太好
     texts: [
         "送给我",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
-        "心爱的姑娘",  // 同上...
+        "最最亲爱的Jerry布布猪",  // 同上...
         "今天是你的生日",
-        "这是我们在一起的",
-        "第三个生日！",
-        "这两年来，我见过你",
+        "这是我陪宝宝一起过的",
+        "第一个生日！",
+        "这一年来，我见过你",
+        "帅帅的样子"
         "奇怪的样子",
         "可爱的样子",
         "生气的样子",
         "都是我喜欢的样子",
-        "对了，礼物收全了吗？",
-        "今年要吃好好的",
-        "喂饱饱的",
+        "很遗憾我没有陪在你的身边",
+        "但是我的祝福和爱会一直在哟",
+        "今年要健健康康的",
+        "吃饱饱的",
         "然后才有力气干活",
-        "和想我！",
+        "还有想我！",
         "生日快乐~~",
     ],
     /**
@@ -31,20 +33,22 @@ var config = {
      */
     imgs: {
         "送给我": "./imgs/songxinxin.jpeg",
-        "心爱的姑娘": "./imgs/nothing.jpg",
+        "最最亲爱的Jerry布布猪": "./imgs/nothing.jpg",
         "今天是你的生日": "./imgs/shengri.jpeg",
-        "这是我们在一起的": "./imgs/zaiyiqi.png",
-        "第三个生日！": "./imgs/nothing.jpg",
-        "这两年来，我见过你": "./imgs/yangzi.jpeg",
+        "这是我陪宝宝一起过的": "./imgs/zaiyiqi.png",
+        "第一个生日！": "./imgs/nothing.jpg",
+        "这一年来，我见过你": "./imgs/yangzi.jpeg",
+        "帅帅的样子": "./imgs/nothing.jpg",
         "奇怪的样子": "./imgs/nothing.jpg",
         "可爱的样子": "./imgs/nothing.jpg",
         "生气的样子": "./imgs/nothing.jpg",
         "都是我喜欢的样子": "./imgs/nothing.jpg",
-        "对了，礼物收全了吗？": "./imgs/chuo.gif",
-        "今年要吃好好的": "./imgs/chihaodian.jpeg",
-        "喂饱饱的": "./imgs/weibao.jpeg",
+        "很遗憾不能在这个重要的日子陪在你身边": "./imgs/chuo.gif",
+        "但是我的祝福和爱会一直在哟": "./imgs/chihaodian.jpeg",
+        "今年要健健康康的": "./imgs/chihaodian.jpeg",
+        "吃饱饱的": "./imgs/weibao.jpeg",
         "然后才有力气干活": "./imgs/nothing.jpg",
-        "和想我！": "./imgs/nothing.jpg",
+        "还有想我！": "./imgs/nothing.jpg",
         "生日快乐~~": "./imgs/nothing.jpg"
     },
     
@@ -61,5 +65,5 @@ var config = {
     },
 
     // 结束语
-    loveText: '爱你的X先生~'
+    loveText: '超爱你的一二宝~'
 };
